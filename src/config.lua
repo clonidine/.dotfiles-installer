56a169aab6bcf46cf380c3b09fce0e3ba311700f
clonidine <lcly@duck.com>
2024-02-17 18:13:28 +0000

    chore: import `utils.lua`

--- src/config.lua
+++ src/config.lua
@@ -1,17 +1,9 @@
-local os = require("os")
+local utils = require("./utils.lua")
 
-DEFAULT_CONFIG_PATH = "~/.config"
+local DEFAULT_CONFIG_PATH = "~/.config"
 
-BASE_REPOSITORY = "https://github.com/clonidine/.dotfiles.git"
-BASE_REPOSITORY_NAME = ".dotfiles"
-
-local function create_dir(name)
-  os.execute(string.format("mkdir -p %s", name))
-end
-
-local function clone_repository()
-  os.execute(string.format("git clone %s", BASE_REPOSITORY))
-end
+local BASE_REPOSITORY = "https://github.com/clonidine/.dotfiles.git"
+local BASE_REPOSITORY_NAME = ".dotfiles"
 
 local function copy_config(path_src, path_target)
   os.execute(string.format("cp -r %s/* %s", path_src, path_target))
@@ -19,10 +11,10 @@ end
 
 function load()
   -- CREATE CONFIG DIRECTORY IF NOT EXISTS
-  create_dir(DEFAULT_CONFIG_PATH)
+  utils.create_dir(DEFAULT_CONFIG_PATH)
 
   -- CLONE REPOSITORY: https://github.com/clonidine/.dotfiles.git
-  clone_repository()
+  utils.clone_repository(BASE_REPOSITORY)
 
   -- COPY CONFIGS FROM BASE REPOSITORY (https://github.com/clonidine/.dotfiles.git) TO CONFIG DIRECTORY
   copy_config(string.format("%s", BASE_REPOSITORY_NAME), DEFAULT_CONFIG_PATH)

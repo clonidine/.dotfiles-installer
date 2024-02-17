4aea867cc9b64694fd6b5b69a334007ffa7e3139
clonidine <lcly@duck.com>
2024-02-17 17:33:26 +0000

    chore: create `load()` function

--- src/config.lua
+++ src/config.lua
@@ -17,11 +17,13 @@ local function copy_config(path_src, path_target)
   os.execute(string.format("cp -r %s/* %s", path_src, path_target))
 end
 
--- CREATE CONFIG DIRECTORY IF NOT EXISTS
-create_dir(DEFAULT_CONFIG_PATH)
+function load()
+  -- CREATE CONFIG DIRECTORY IF NOT EXISTS
+  create_dir(DEFAULT_CONFIG_PATH)
 
--- CLONE REPOSITORY: https://github.com/clonidine/.dotfiles.git
-clone_repository()
+  -- CLONE REPOSITORY: https://github.com/clonidine/.dotfiles.git
+  clone_repository()
 
--- COPY CONFIGS FROM BASE REPOSITORY (https://github.com/clonidine/.dotfiles.git) TO CONFIG DIRECTORY
-copy_config(string.format("%s", BASE_REPOSITORY_NAME), DEFAULT_CONFIG_PATH)
+  -- COPY CONFIGS FROM BASE REPOSITORY (https://github.com/clonidine/.dotfiles.git) TO CONFIG DIRECTORY
+  copy_config(string.format("%s", BASE_REPOSITORY_NAME), DEFAULT_CONFIG_PATH)
+end

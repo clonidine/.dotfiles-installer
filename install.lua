cf39dee4090c0e7ab487cf5559b3bde047c998ca
clonidine <lcly@duck.com>
2024-02-17 16:55:45 +0000

    chore: remove `dir_exists` function

--- install.lua
+++ install.lua
@@ -1,25 +1,12 @@
 local os = require("os")
 
-DEFAULT_CONFIG_PATH = "~/.config"
+DEFAULT_CONFIG_PATH = "~/Documents/config"
 
 BASE_REPOSITORY = "https://github.com/clonidine/.dotfiles.git"
 BASE_REPOSITORY_NAME = ".dotfiles"
 
-local function dir_exists(file)
-  local ok, err, code = os.rename(file, file)
-  local permission_denied = 13
-  if not ok then
-    if code == permission_denied then
-      return true
-    end
-  end
-  return ok, err
-end
-
 local function create_dir(name)
-  if not dir_exists(name) then
-    os.execute(string.format("mkdir %s", name))
-  end
+  os.execute(string.format("mkdir -p %s", name))
 end
 
 local function clone_repository()
